cd08c89d3b856ecffbe937aeece6fa871483dae4
Brian Cadavid <briancadavid@outlook.com>
2025-05-09 21:42:07 -0500

    Update Crear Base de datos.bat

--- Crear Base de datos.bat
+++ Crear Base de datos.bat
@@ -1,6 +1,10 @@
 @echo off
 setlocal
 
+echo ============================
+echo INICIANDO PROCESO EF MIGRATION
+echo ============================
+
 echo Restaurando paquetes...
 dotnet restore
 
@@ -9,6 +13,12 @@ dotnet build SggApp\SggApp.csproj
 IF ERRORLEVEL 1 (
     echo Error en la compilación. Abortando...
     exit /b 1
+)
+
+:: Comprobar si ya existe la carpeta de migraciones
+IF NOT EXIST "SistemaFactura.DAL\Migrations" (
+    echo Creando migración inicial...
+    dotnet ef migrations add Inicial --project SistemaFactura.DAL --startup-project SggApp
 )
 
 echo Aplicando migraciones y creando la base de datos...
